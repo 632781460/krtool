105034a50183d583b4c3e789e642554f94b017ff
songkaiyu <songkaiyu@36kr.com>
2016-07-12 14:38:06 +0800

    fix: resverse tags

--- ktool-hotfix.js
+++ ktool-hotfix.js
@@ -5,7 +5,8 @@ const semver = require('semver');
 checkoutFix();
 function checkoutFix() {
     git.tags(function listTags(err, tags) {
-        tags = tags.all;
+        tags = tags.all || [];
+        tags.reverse();
         var tag = 'master';
         for (var i = 0, l = tags.length; i < l; i++) {
             if (semver.valid(tags[i])) {
