d0dd96cc5a33eedd2992441744111bf404aec458
xiaobao <luxinyue@36kr.com>
2016-07-14 11:51:39 +0800

    feat: 增加branch命令，功能与hotfix一致

--- cli.js
+++ cli.js
@@ -7,6 +7,7 @@ const rootPath = findRoot(process.cwd());
 const pkgPath = path.join(rootPath, 'package.json');
 const pkg = require(pkgPath);
 program.version(pkg.version)
+	.command('branch <branch>', '从最新的tag打出feature分支').alias('f')
 	.command('hotfix <branch>', '从最新的tag打出fix分支').alias('h')
 	.command('tag <tag>', '生成发布标签，CHANGELOG').alias('t')
 	.parse(process.argv);
