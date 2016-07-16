e928bddaf619a1eb96bb5a475924f5b1f5382af8
songkaiyu <songkaiyu@36kr.com>
2016-07-16 11:44:46 +0800

    fix(hotfix): 切分支之前fetch，从最大版本，切出分支

--- krtool-hotfix.js
+++ krtool-hotfix.js
@@ -6,16 +6,22 @@ const program = require('commander');
 program.parse(process.argv);
 checkoutFix();
 function checkoutFix() {
-    git.tags(function listTags(err, tags) {
+    git.fetch()
+    .tags(function listTags(err, tags) {
         tags = tags.all || [];
         tags.reverse();
-        var tag = 'master';
-        for (var i = 0, l = tags.length; i < l; i++) {
-            if (semver.valid(tags[i])) {
+        const INIT_VERSION = '0.0.0';
+        var tag = INIT_VERSION;
+        for (var i = 1, l = tags.length; i < l; i++) {
+            if (semver.valid(tags[i]) && semver.gt(tags[i], tag)) {
                 tag = tags[i];
-                break;
             }
         }
+
+        if (tag === INIT_VERSION) {
+            tag = 'master';
+        }
+
         var krfix = program.args[0] || 'krfix';
         git.checkoutBranch(krfix, tag);
     });
