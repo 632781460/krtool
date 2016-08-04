229e3c761f3b889cd74e64ad827fcf392e74e2e3
songkaiyu <songkaiyu@36kr.com>
2016-08-04 19:10:02 +0800

    fix(version): 修复版本获取错误

--- cli.js
+++ cli.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const findRoot = require('find-root');
 const path = require('path');
-const rootPath = findRoot(process.cwd());
-const pkgPath = path.join(rootPath, 'package.json');
+const pkgPath = path.join(__dirname, 'package.json');
 const pkg = require(pkgPath);
 program.version(pkg.version)
     .command('branch <branch>', '从最新的tag打出feature分支').alias('f')
